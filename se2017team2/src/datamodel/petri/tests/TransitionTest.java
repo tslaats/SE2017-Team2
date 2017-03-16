7979df79ee94ec92da156c0f182cef6e6f2bdbe4
Simon Gustav Cordes <simon_cordes@hotmail.com>
2017-03-16 09:28:43 +0100

    Finalized tests for the Transition class

---
+++ se2017team2/src/datamodel/petri/tests/TransitionTest.java
@@ -0,0 +1,57 @@
+package datamodel.petri.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import datamodel.Position;
+import datamodel.cr.CrGraph;
+import datamodel.petri.Transition;
+
+public class TransitionTest {
+
+	private static final String testname = "test";
+	
+	private static Transition transition;
+	
+	private static Position pos = new Position(2,3);
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		transition = new Transition(pos, testname);
+	}
+
+	@Test
+	public void testTransition() {
+		assertTrue(transition.getCanBeDeleted()
+				&& transition.getPos().x() == 2
+				&& transition.getPos().y() == 3
+				&& transition.getName() == testname);
+	}
+
+	@Test
+	public void testSetName() {
+		String newname = "newname";
+		
+		transition.setName(newname);
+	
+		assertTrue(transition.getName() == newname);
+	}
+
+	@Test
+	public void testSetCrGraph() {
+		String newname = "newname";
+		
+		CrGraph crgraph = new CrGraph(newname);
+		int id = crgraph.getID();
+		
+		transition.setCrGraph(crgraph);
+		
+		assertTrue(transition.getCrGraph().getID() == id);
+	}
+
+}
