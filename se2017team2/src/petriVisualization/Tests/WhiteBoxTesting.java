f94fb2ce01724c8a31bdfc5f871cf44a0d470a45
Tobias <tobiascarlostv@gmail.com>
2017-03-28 14:29:32 +0200

    Update to WhiteBoxTesting

--- se2017team2/src/petriVisualization/Tests/WhiteBoxTesting.java
+++ se2017team2/src/petriVisualization/Tests/WhiteBoxTesting.java
@@ -23,18 +23,35 @@ public class WhiteBoxTesting {
 		testPetriConverter();
 	}
 	
-	private static void testPetriWindow(){
+private static void testPetriWindow(){
 		
 		ArrayList<Node> nodes = new ArrayList<Node>();
 		ArrayList<Edge> edges = new ArrayList<Edge>();
 		
         Node place1 = new Node( "Place", new Point(100,100));
         Node transition1 = new Node("Transition", new Point(300,100));
+        
+        Node place2 = new Node( "Place", new Point(200,200));
+        Node transition2 = new Node("Transition", new Point(200,400));
+        
+        Node place3 = new Node( "Place", new Point(500,500));
+        Node transition3 = new Node("Transition", new Point(300,600));
+
 
         nodes.add(place1);
         nodes.add(transition1);
+        
+        nodes.add(place2);
+        nodes.add(transition2);
+        
+        nodes.add(place3);
+        nodes.add(transition3);
 
         edges.add(new Edge(place1, transition1));
+        edges.add(new Edge(transition1, place2));
+        edges.add(new Edge(place2, transition2));
+        edges.add(new Edge(transition2, place3));
+        edges.add(new Edge(place3, transition3));
 
         PetriWindow petriWindow = new PetriWindow(true);
         petriWindow.setVisible(true);
